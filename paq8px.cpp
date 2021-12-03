3030353f2c43c88204b2abac447335653b8da4eb
Zoltán Gotthardt <gotty@freemail.hu>
2021-12-04 00:00:33 +0100

    fix1: fixed reading filesize from archive greater then 4gb. Renamed utils.hpp to Utils.hpp to fix build on some linux systems.

--- paq8px.cpp
+++ paq8px.cpp
@@ -7,7 +7,7 @@
 //////////////////////// Versioning ////////////////////////////////////////
 
 #define PROGNAME     "paq8px-lite"
-#define PROGVERSION  "-t2"  //update version here before publishing your changes
+#define PROGVERSION  "-t2-fix1"  //update version here before publishing your changes
 #define PROGYEAR     "2021"
 
 
